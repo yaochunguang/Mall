d69125676cd45bc0145b6427f02129cb0910e4ac
yaochunguang <18819458116@163.com>
2019-06-30 20:38:20 +0800

    导入dao和parent项目

---
+++ mall-dao/src/main/java/com/company/mall/mapper/TbBrandMapper.java
@@ -0,0 +1,31 @@
+package com.company.mall.mapper;
+
+import com.company.mall.pojo.TbBrand;
+import com.company.mall.pojo.TbBrandExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface TbBrandMapper {
+    int countByExample(TbBrandExample example);
+
+    int deleteByExample(TbBrandExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbBrand record);
+
+    int insertSelective(TbBrand record);
+
+    List<TbBrand> selectByExample(TbBrandExample example);
+
+    TbBrand selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbBrand record, @Param("example") TbBrandExample example);
+
+    int updateByExample(@Param("record") TbBrand record, @Param("example") TbBrandExample example);
+
+    int updateByPrimaryKeySelective(TbBrand record);
+
+    int updateByPrimaryKey(TbBrand record);
+}
